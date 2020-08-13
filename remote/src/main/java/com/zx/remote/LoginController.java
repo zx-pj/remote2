c32d31652a6c7766c3dff1b55f279a26a30d3c9f
zx <1025550922@qq.com>
2020-08-13 16:00:11 +0800

    第一次提交

---
+++ remote/src/main/java/com/zx/remote/LoginController.java
@@ -0,0 +1,13 @@
+package com.zx.remote;
+
+import org.springframework.stereotype.Controller;
+
+import java.sql.SQLOutput;
+
+
+public class LoginController {
+    public static void main(String[] args) {
+        System.out.println("hhh");
+    }
+
+}
---
+++ remote/src/test/java/com/zx/remote/RemoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zx.remote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
