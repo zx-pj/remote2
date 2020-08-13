a07af8a27f7fa25a69062d0338d5e921532d2658
zx <1025550922@qq.com>
2020-08-13 16:05:08 +0800

    第二次提交

--- remote/src/main/java/com/zx/remote/LoginController.java
+++ remote/src/main/java/com/zx/remote/LoginController.java
@@ -7,7 +7,7 @@ import java.sql.SQLOutput;
 
 public class LoginController {
     public static void main(String[] args) {
-        System.out.println("hhhwwwwwwwwwwwwww");
+        System.out.println("hhhwwwwwwwwwwwwwwlllllllll");
     }
 
 }
